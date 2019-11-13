809fe15ae5bce298ba0d90fdecab6ba7446a1a3f
chihwan <clghks@gmail.com>
2019-11-13 14:16:45 +0900

    북마크 기능 개발 완료

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Bookmark
 
 
 # Create your views here.
 class BookmarkListView(ListView):
     model = Bookmark
+
+
+class BookmarkCreateView(CreateView):
+    model = Bookmark
+    fields = ['site_name', 'url']
+    success_url = reverse_lazy('list')
+
+
+class BookmarkDetailView(DetailView):
+    model = Bookmark
+
+
+class BookmarkUpdateView(UpdateView):
+    model = Bookmark
+    fields = ['site_name', 'url']
+    template_name_suffix = '_update'
+    success_url = reverse_lazy('list')
+
+
+class BookmarkDeleteView(DeleteView):
+    model = Bookmark
+    fields = ['site_name', 'url']
+    success_url = reverse_lazy('list')
