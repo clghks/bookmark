5a4675c09d5768db8ddc36467be6260d58689d86
chihwan <clghks@gmail.com>
2019-11-13 17:11:20 +0900

    배포환경 설정

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e!_c)6psoc68gs-_4h*aprqf$%#%rw%8yct^t^596ck3q5$^mk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
